e0a5eb29df5193e3f20ea7db14cfa3d359c8be79
Alex Morega <alex@grep.ro>
2015-07-17 14:53:17 +0200

    blank homepage

--- fantani/urls.py
+++ fantani/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from campaign import views
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index),
 ]
