671afc028727b2fc0273b7935f007750b749e1e9
Alex Morega <alex@grep.ro>
2015-07-17 16:05:30 +0200

    gis model admin

--- campaign/admin.py
+++ campaign/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from campaign.models import Entry
 
-# Register your models here.
+
+class EntryAdmin(admin.OSMGeoAdmin):
+    default_lat = 5532000
+    default_lon = 2905000
+    default_zoom = 12
+
+admin.site.register(Entry, EntryAdmin)
