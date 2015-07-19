40efd0ae1d75c2da1bf57df94c411032fd85d053
Alex Morega <alex@grep.ro>
2015-07-19 18:35:22 +0300

    set DJANGO_SETTINGS_MODULE earlier

--- fantani/wsgi.py
+++ fantani/wsgi.py
@@ -1,8 +1,9 @@
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantani.settings")
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
